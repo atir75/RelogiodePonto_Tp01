896255898ed4df4ec6b5bf4936b34abe78275337
atir75 <maria.rita27012@gmail.com>
2022-10-11 11:04:34 -0300

    Adicionar arquivos de projeto.

---
+++ Tp01/Ponto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tp01
+{
+    public class Ponto
+    {
+        public String matricula;
+        public String data;
+        public String hora;
+        public String personalizado;
+
+
+        public void setMatricula(String _matricula) { matricula = _matricula; }
+        public void setData(String _data) { data = _data; }
+        public void setHora(String _hora) { hora = _hora; }
+        public void setPersonalizado(String _personalizado) { personalizado = _personalizado; }
+
+
+
+        public String getMatricula() { return matricula; }
+        public String getData() { return data; }
+        public String getHora() { return hora; }
+        public String getPersonalizado() { return personalizado; }
+
+    }
+}
---
+++ Tp01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Tp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader("abril2017.txt");
+            Ponto umponto = new Ponto();
+            String buffer;
+            List<Ponto> ponto = new List<Ponto>();
+            int i = 0;
+
+            buffer = file.ReadLine();
+            while (buffer != null)
+            {
+                umponto.setMatricula(buffer.Substring(0, 15));
+                umponto.setData(buffer.Substring(15, 6));
+                umponto.setHora(buffer.Substring(21, 4));
+                umponto.setPersonalizado(buffer.Substring(25));
+                ponto.Add(new Ponto());
+                ponto[i] = umponto;
+                i++;
+
+                buffer = file.ReadLine();
+            }
+            TextWriter arquivo = new StreamWriter("ponto.xml");
+            XmlSerializer obj = new XmlSerializer(ponto.GetType());
+
+            obj.Serialize(arquivo, ponto);
+            arquivo.Close();
+            Console.ReadKey();
+        }
+    }
+}
